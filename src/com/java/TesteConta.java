6d9965a82b47407f7323ae1bfad1eb93fdc1b7ab
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2021-11-23 20:55:34 -0300

    Add new construtors in conta class

--- src/com/java/TesteConta.java
+++ src/com/java/TesteConta.java
@@ -3,29 +3,10 @@ package com.java;
 public class TesteConta {
 
 	public static void main(String[] args) {
-		Conta contaCorrente = new Conta();
-		Conta contaPoupanca = new Conta();
-		Conta contaInvestimento = new Conta();
+		Conta cc = new Conta(1000, 50.0);
 		
-		contaCorrente.numero = 11;
-		contaCorrente.saldo = 20.5;
-		contaCorrente.cliente.nome = "Rodrigo";
-		contaCorrente.cliente.idade = 29;
-				
-		contaPoupanca.numero = 22;
-		contaPoupanca.saldo = 30;
-		contaPoupanca.cliente.nome = "Deborah";
-		contaPoupanca.cliente.idade = 35;
+		System.out.println(cc.recuperarSaldo());
 		
-		contaInvestimento.numero = 33;
-		contaInvestimento.saldo = 40;
-		contaInvestimento.cliente.nome = "Luccca";
-		contaInvestimento.cliente.idade = 02;
-		
-		System.out.println(contaCorrente.numero);
-		System.out.println(contaCorrente.saldo);
-		System.out.println(contaCorrente.cliente.nome);
-		System.out.println(contaCorrente.cliente.idade);
 		
 	}
 
